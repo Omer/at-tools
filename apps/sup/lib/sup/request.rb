40941aa16dbb4464a1f4721a29753737634d1126
Stephen McGruer <stephen_mcgruer@gmail.com>
2010-09-17 17:50:14 +0100

    Retabbed request.rb to use 2 spaces

--- apps/sup/lib/sup/request.rb
+++ apps/sup/lib/sup/request.rb
@@ -3,60 +3,60 @@ require "net/http"
 
 class RequestException < RuntimeError
 
-    attr_reader :message
+  attr_reader :message
 
-    def initialize message
+  def initialize message
 
-        @message = message
+    @message = message
 
-    end
+  end
 
 end
 
 class Request
 
-	attr_accessor :username, :machine, :email, :urgent, :subject, :description
-		
-	def initialize
-		@username = ENV['LOGNAME'] || ENV['USERNAME'] || ENV['USER']
-		@machine = ENV['SHORT_HOSTNAME']
-		@email = nil
-		@urgent = 0
-	end
+  attr_accessor :username, :machine, :email, :urgent, :subject, :description
+        
+  def initialize
+    @username = ENV['LOGNAME'] || ENV['USERNAME'] || ENV['USER']
+    @machine = ENV['SHORT_HOSTNAME']
+    @email = nil
+    @urgent = 0
+  end
 
-	def submit!(url = "http://www.tardis.ed.ac.uk/~ediblespread/rubytest/test2.php")
+  #def submit!(url = "http://www.tardis.ed.ac.uk/~ediblespread/rubytest/test2.php")
 
-    #def submit!(url = "http://www.inf.ed.ac.uk/cgi-bin/send-sup-form.cgi")
+  def submit!(url = "http://www.inf.ed.ac.uk/cgi-bin/send-sup-form.cgi")
 
-		# Fire off the request. Currently uses a test php file located on my
-		# (Stephen McGruer) website which merely echos the POST values.
-		#
-		# We need to find out how to test that we're addressing the "urgent"
-		# keyword properly without spamming CS. Maybe ask them what they try and
-		# match?
-		#
-		# Will also need to guard against 404, 403, no connection, etc.
-		params = {
-		  :username => self.username,
-		  :machine_name => self.machine,
-		  :email => self.email,
-		  :short_message => self.subject,
-		  :urgent => self.urgent,
-		  :long_message => self.description
-		}
+    # Fire off the request. Currently uses a test php file located on my
+    # (Stephen McGruer) website which merely echos the POST values.
+    #
+    # We need to find out how to test that we're addressing the "urgent"
+    # keyword properly without spamming CS. Maybe ask them what they try and
+    # match?
+    #
+    # Will also need to guard against 404, 403, no connection, etc.
+    params = {
+      :username => self.username,
+      :machine_name => self.machine,
+      :email => self.email,
+      :short_message => self.subject,
+      :urgent => self.urgent,
+      :long_message => self.description
+    }
 
-        begin
-    		response = Net::HTTP.post_form(URI.parse(url), params)
-        rescue Net::HTTPExceptions => e 
-            raise RequestException.new(e.message)
-        rescue SocketError => e
-            raise RequestException.new(e.message)
-        end
+    begin
+      response = Net::HTTP.post_form(URI.parse(url), params)
+    rescue Net::HTTPExceptions => e 
+      raise RequestException.new(e.message)
+    rescue SocketError => e
+      raise RequestException.new(e.message)
+    end
 
-        raise RequestException.new("RequestException: Request failed (response code: #{response.code})") unless response.code.eql? "200"
+    raise RequestException.new("RequestException: Request failed (response code: #{response.code})") unless response.code.eql? "200"
 
-        response
+    response
 
-	end
+  end
 
 end
