42decaedc3f366a54606c4e95caa65d4deb1d421
Stephen McGruer <stephen_mcgruer@gmail.com>
2010-09-17 15:26:43 +0100

    Refactored sup to use a request object.
    It's terrible practice, being one big commit,
    but sup has been refactored to use a request
    object.
    
    The object creates and stores the variables for
    sup (username etc), and also handles the submission
    through the submit method.

---
+++ apps/sup/lib/sup.rb
@@ -0,0 +1,4 @@
+SUP_ROOT = File.join(File.dirname(Pathname.new(__FILE__).realpath), '/sup')
+
+require SUP_ROOT + '/request'
+
---
+++ apps/sup/lib/sup/request.rb
@@ -0,0 +1,35 @@
+class Request
+
+	attr_accessor :username, :machine, :email, :urgent, :subject, :description
+		
+	def initialize
+		@username = ENV['LOGNAME'] || ENV['USERNAME'] || ENV['USER']
+		@machine = ENV['SHORT_HOSTNAME']
+		@email = nil
+		@urgent = 0
+	end
+
+	def submit(url = "http://www.tardis.ed.ac.uk/~ediblespread/rubytest/test.php")
+
+		# Fire off the request. Currently uses a test php file located on my
+		# (Stephen McGruer) website which merely echos the POST values.
+		#
+		# We need to find out how to test that we're addressing the "urgent"
+		# keyword properly without spamming CS. Maybe ask them what they try and
+		# match?
+		#
+		# Will also need to guard against 404, 403, no connection, etc.
+		params = {
+		  :username => self.username,
+		  :machine_name => self.machine,
+		  :email => self.email,
+		  :short_message => self.subject,
+		  :urgent => self.urgent,
+		  :long_message => self.description
+		}
+		
+		response = Net::HTTP.post_form(URI.parse(url), params)
+
+	end
+
+end
