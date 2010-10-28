848ea267827d8cbc7ccb3c10103fb65fe47559d6
Stephen McGruer <stephen_mcgruer@gmail.com>
2010-10-28 21:22:12 +0100

    Added puts back to parser.rb

--- apps/carto/lib/parser.rb
+++ apps/carto/lib/parser.rb
@@ -7,12 +7,12 @@ include REXML
 PROFILENAME = APP_ROOT + '/hosts/inventory.xml'
 YAMLNAME = APP_ROOT + '/hosts/inventory.yaml'
 
-#puts ">>> Carto library loaded. Getting information..."
+puts ">>> Carto library loaded. Getting information..."
 def self.get_inventory
 	unless FileTest.exist?(YAMLNAME) and File.mtime(YAMLNAME) > (Time.now - 86400)
 		# check for net connection
 		# unless `ping -t 5 lcfg.inf.ed.ac.uk`
-			#puts ">>> Inventory out of date. Reacquiring data from the server..."
+			puts ">>> Inventory out of date. Reacquiring data from the server..."
 			unless FileTest.exist?(PROFILENAME)
 				Net::HTTP.start('lcfg.inf.ed.ac.uk') { |http|
 					resp = http.get('/profiles/inf.ed.ac.uk/inventory/XMLInventory/profile.xml')
@@ -20,14 +20,14 @@ def self.get_inventory
 						file.write(resp.body)
 					}
 				}
-				#puts ">>> Reacquisition complete."
+				puts ">>> Reacquisition complete."
 			end
 			return parse_me( Document.new(File.new(PROFILENAME)) )
 		#else 
 		#	puts "No net connection. Falling back to saved file."
 		#end
 	end
-	#puts ">>> Inventory loaded. <<<"
+	puts ">>> Inventory loaded. <<<"
 	return YAMLNAME
 end
 
@@ -63,4 +63,4 @@ def self.inventory
 	@inventory ||= get_inventory
 end
 inventory
-#puts ">>> Loading complete. Carto initialized and ready..."
+puts ">>> Loading complete. Carto initialized and ready..."
